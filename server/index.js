a13c09df6366a7719b62f643c729ea6cc7d1bc5f
mdmonis <mdmonis128@gmail.com>
2025-01-29 16:07:07 +0530

    A server that stores log

--- server/index.js
+++ server/index.js
@@ -1,8 +1,26 @@
 const http = require("http");
+const fs = require("fs");
 
 const myServer = http.createServer((req,res)=>{
-    console.log(req);
-    res.end("req cannot be passsed here");
+    const log = `${Date.now()}: Request Received at ${req.url}\n`
+
+    //Always use non blocking requests
+    fs.appendFile("log.txt", log,(err,data)=>{
+        switch(req.url){
+            case '/about':
+                res.end("Salam Alaykum I am Monis.")
+                break;
+            case '/':
+                res.end("HomePage");
+                break;
+            default:
+                res.end("404 not found");    
+
+        }
+    })
+    
+    // console.log(req);
+    // res.end("req cannot be passsed here");
 })
 
 myServer.listen(8000,()=>console.log("Server Started"));
