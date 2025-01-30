3c4212d607eb74a5cec5c6dcf7dca9468e60cd41
mdmonis <mdmonis128@gmail.com>
2025-01-30 13:17:06 +0530

    Search Query processed through url

--- server/index.js
+++ server/index.js
@@ -1,14 +1,20 @@
 const http = require("http");
 const fs = require("fs");
+const url = require("url");
 
 const myServer = http.createServer((req,res)=>{
+    if(req.url=='/favicon.ico') return res.end();
+
+    const myUrl = url.parse(req.url,true);
+    console.log(myUrl);
     const log = `${Date.now()}: Request Received at ${req.url}\n`
 
     //Always use non blocking requests
     fs.appendFile("log.txt", log,(err,data)=>{
-        switch(req.url){
+        switch(myUrl.pathname){
             case '/about':
-                res.end("Salam Alaykum I am Monis.")
+                const username = myUrl.query.username;
+                res.end("Salam Alaykum I am"+username);
                 break;
             case '/':
                 res.end("HomePage"); 
