00eb00f3b25e1d783f57009da7aec78818ba17ba
Md <mdmonis128@gmail.com>
2025-02-01 21:43:36 +0530

    Dynamic Routing and Route Grouping Achieved

--- project-01/index.js
+++ project-01/index.js
@@ -4,4 +4,37 @@ const users = require("./MOCK_DATA.json");
 
 const PORT = 8000;
 
+//Routes
+app.get("/users", (req, res) => {
+  const html = `
+    <ul>
+    ${users.map((user) => ` <li>${user.first_name}</li>`).join("--------")}    
+    </ul>    
+    `;
+  res.send(html);
+});
+
+//REST API
+app.get("/api/users", (req, res) => {
+  return res.json(users);
+});
+
+
+app.route("/api/users/:id").get("/api/users/:id", (req, res) => {
+    const id = Number(req.params.id);
+    const user = users.find((user) => user.id === id);
+    res.send(user);
+  }).patch((req,res)=>{
+    return res.json({status: "pending"})
+  }).post((req,res)=>{
+    return res.json({status: "pending"})
+  }).delete((req,res)=>{
+    return res.json({status: "pending"})
+  })
+// app.get("/api/users/:id", (req, res) => {
+//   const id = Number(req.params.id);
+//   const user = users.find((user) => user.id === id);
+//   res.send(user);
+// });
+
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
