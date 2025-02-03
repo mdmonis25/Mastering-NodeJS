f8b30562b8796a6f611c127cd65bacc6b258a274
Md <mdmonis128@gmail.com>
2025-02-03 11:53:01 +0530

    Updated Post Route

--- project-01/index.js
+++ project-01/index.js
@@ -1,8 +1,12 @@
 const express = require("express");
 const app = express();
 const users = require("./MOCK_DATA.json");
+const fs = require("fs");
 
 const PORT = 8000;
+
+//Middleware - plugins in express
+app.use(express.urlencoded({ extended: false }));
 
 //Routes
 app.get("/users", (req, res) => {
@@ -19,22 +23,29 @@ app.get("/api/users", (req, res) => {
   return res.json(users);
 });
 
-
-app.route("/api/users/:id").get((req, res) => {
+app
+  .route("/api/users/:id")
+  .get((req, res) => {
     const id = Number(req.params.id);
     const user = users.find((user) => user.id === id);
     res.send(user);
-  }).patch((req,res)=>{
-    return res.json({status: "pending"})
-  }).post((req,res)=>{
-    return res.json({status: "pending"})
-  }).delete((req,res)=>{
-    return res.json({status: "pending"})
   })
-// app.get("/api/users/:id", (req, res) => {
-//   const id = Number(req.params.id);
-//   const user = users.find((user) => user.id === id);
-//   res.send(user);
-// });
+  .patch((req, res) => {
+    return res.json({ status: "pending" });
+  })
+  .post((req, res) => {
+    const body = req.body;
+    users.push({id:users.length+1,...body})
+    fs.writeFile("./MOCK_DATA.json", JSON.stringify(users),(err,data)=>{
+      return res.send(`User with id = ${users.length} added`);
+    })
+  })
+  .delete((req, res) => {
+    const id = Number(req.params.id);
+    const user = users.find((user) => user.id === id);
+    fs.unlink("./MOCK_DATA.json",JSON.stringify(users),())
+    return res.json({ status: "pending" });
+  });
+
 
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
