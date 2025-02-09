15572f7a306f519bd14d441afbacbd98a9727d96
mdmonis <mdmonis128@gmail.com>
2025-02-09 13:20:16 +0530

    post route improved

--- project-01/index.js
+++ project-01/index.js
@@ -7,15 +7,15 @@ const PORT = 8000;
 
 //Middleware - plugins in express
 app.use(express.urlencoded({ extended: false }));
-app.use((req,res,next)=>{
+app.use((req, res, next) => {
   console.log("Middle Ware 1");
   next();
-})
-app.use((req,res,next)=>{
+});
+app.use((req, res, next) => {
   console.log("Middle Ware 2");
   // return res.status(786).json({mid:"middleware"})
   next();
-})
+});
 
 //Routes
 app.get("/users", (req, res) => {
@@ -29,8 +29,17 @@ app.get("/users", (req, res) => {
 
 //REST API
 app.get("/api/users", (req, res) => {
-  res.setHeader("X-MyName","Monis")
+  res.setHeader("X-MyName", "Monis");
   return res.json(users);
+});
+
+app.post("/api/users", (req, res) => {
+  const body = req.body;
+  let newId = users[users.length - 1].id + 1;
+  users.push({ id: newId, ...body });
+  fs.writeFile("./MOCK_DATA.json", JSON.stringify(users), (err, data) => {
+    return res.send(`User with id = ${newId} added`);
+  });
 });
 
 app
@@ -43,21 +52,14 @@ app
   .patch((req, res) => {
     return res.json({ status: "pending" });
   })
-  .post((req, res) => {
-    const body = req.body;
-    users.push({id:users.length+1,...body})
-    fs.writeFile("./MOCK_DATA.json", JSON.stringify(users),(err,data)=>{
-      return res.send(`User with id = ${users.length} added`); 
-    })
-  })
   .delete((req, res) => {
     const id = Number(req.params.id);
-    if(!(users.find(item => item.id === id))) return res.send(`User with id = ${id} not found`);
-    const user = users.filter((user) => user.id !== id);
-    fs.writeFile("./MOCK_DATA.json",JSON.stringify(user),(err,data)=>{
-      return res.send(`User with id = ${id} deleted`)
-    })
+    if (!users.find((item) => item.id === id))
+      return res.send(`User with id = ${id} not found`);
+    let user = users.filter((user) => user.id !== id);
+    fs.writeFile("./MOCK_DATA.json", JSON.stringify(user), (err, data) => {
+      return res.send(`User with id = ${id} deleted`);
+    });
   });
-
 
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
